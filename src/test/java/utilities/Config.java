604a01f4134a6bf6ad1fee4b013520a04cfa2d97
Karina <karina@Karinas-MacBook-Pro-2.local>
2024-01-10 13:22:51 -0500

    Committing local changes before pulling clear

--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -11,7 +11,7 @@ public class Config {
     static {
         properties = new Properties();
         try {
-            FileInputStream file = new FileInputStream("src\\test\\resources\\configuration.properties");
+            FileInputStream file = new FileInputStream("src/test/resources/configuration.properties");
             properties.load(file);
             file.close();
         } catch (IOException e) {
